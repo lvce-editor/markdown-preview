5673536fd14e34ffa693021e439931b01aea8951
Le Vivilet <levivlet@pm.me>
2024-09-11 22:31:26 +0200

    fix rename

--- packages/build/src/build.js
+++ packages/build/src/build.js
@@ -44,6 +44,13 @@ await replace({
   replacement: 'dist/markdownPreviewWorkerMain.js',
 })
 
+const markedUrlPath = path.join(root, 'dist', 'markdown-preview-worker', 'src', 'parts', 'MarkedUrl', 'MarkedUrl.ts')
+await replace({
+  path: markedUrlPath,
+  occurrence: `export const markedUrl = new URL('../../../node_modules/marked/lib/marked.esm.js', import.meta.url).toString()`,
+  replacement: `export const markedUrl = new URL('../third_party/marked.esm.js', import.meta.url).toString()`,
+})
+
 const assetDirPath = path.join(root, 'dist', 'src', 'parts', 'AssetDir', 'AssetDir.ts')
 await replace({
   path: assetDirPath,
