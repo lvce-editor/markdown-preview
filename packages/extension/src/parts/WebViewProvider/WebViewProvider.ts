5b81854f8a10675b10954488e479ba4047bfc642
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-09-11 22:20:32 +0200

    feature: render markdown raw content (#5)

--- packages/extension/src/parts/WebViewProvider/WebViewProvider.ts
+++ packages/extension/src/parts/WebViewProvider/WebViewProvider.ts
@@ -1,20 +1,14 @@
 import * as MediaPreviewWorker from '../MarkdownPreviewWorker/MarkdownPreviewWorker.ts'
 
-const previewId = 1
-
 export const webViewProvider = {
   id: 'builtin.markdown-preview',
   async create(webView, uri) {
     // @ts-ignore
     const content = await vscode.readFile(uri)
-    console.log({ content })
-    // TODO if can use remote uri, use remote uri, else read file
-    // @ts-ignore
-    const remoteUrl = await MediaPreviewWorker.invoke('MediaPreview.getUrl', uri)
-    await webView.invoke('initialize', remoteUrl)
+    const html = await MediaPreviewWorker.invoke('MarkdownPreview.render', content)
+    await webView.invoke('initialize', html)
     // @ts-ignore
     webViewProvider.webView = webView
-    await MediaPreviewWorker.invoke('MediaPreview.create', previewId)
   },
   async open(uri, webView) {},
   commands: {},
---
+++ packages/markdown-preview-worker/src/parts/Render/Render.ts
@@ -0,0 +1,3 @@
+export const render = (content) => {
+  return content
+}
