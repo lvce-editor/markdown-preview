17c1a7c47fde2a9fdc9ef5c18de073d35486b717
Le Vivilet <levivlet@pm.me>
2024-09-21 07:24:37 +0200

    feature: add e2e test

--- packages/e2e/src/markdown-preview.js
+++ packages/e2e/src/markdown-preview.js
@@ -1,9 +1,22 @@
 export const name = 'markdown-preview'
 
-export const skip = true
-
 export const test = async ({ FileSystem, Main, Editor, Locator, expect }) => {
-  // TODO verify media preview opens
-  // TODO add tests for different files types
-  // TODO add tests for errors
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(
+    `${tmpDir}/test.md`,
+    `## test markdown
+
+- one
+- two
+- three
+`,
+  )
+
+  // act
+  await Main.openUri(`${tmpDir}/test.md`)
+
+  // assert
+  const webView = Locator('.WebView')
+  await expect(webView).toBeVisible()
 }
