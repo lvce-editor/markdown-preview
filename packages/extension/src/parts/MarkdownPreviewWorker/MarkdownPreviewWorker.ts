ee631d9e6e64fd571d1b5cefa458dfc51bcc46f2
Le Vivilet <levivlet@pm.me>
2025-01-13 22:27:27 +0100

    feature: simplify webview

--- packages/extension/src/parts/MarkdownPreviewWorker/MarkdownPreviewWorker.ts
+++
@@ -1,6 +0,0 @@
-import * as GetOrCreateWorker from '../GetOrCreateWorker/GetOrCreateWorker.ts'
-import * as LaunchMarkdownPreviewWorker from '../LaunchMarkdownPreviewWorker/LaunchMarkdownPreviewWorker.ts'
-
-const { invoke } = GetOrCreateWorker.getOrCreateWorker(LaunchMarkdownPreviewWorker.launchMarkdownPreviewWorker)
-
-export { invoke }
--- packages/markdown-preview-worker/src/parts/Create2/Create2.ts
+++ packages/markdown-preview-worker/src/parts/Create2/Create2.ts
@@ -1,7 +1,7 @@
 import * as Render from '../Render/Render.ts'
 import * as Rpc from '../Rpc/Rpc.ts'
 
-export const create2 = async ({ port, savedState, webViewId, uri }) => {
+export const create2 = async ({ port, uri }) => {
   const content = await Rpc.invoke('WebView.readFile', uri)
   const html = await Render.render(content)
   await port.invoke('initialize', html)
