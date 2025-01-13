b09375bb08a3c2ea8fe901ef2bdf4b2c2bf11184
Le Vivilet <levivlet@pm.me>
2025-01-13 22:19:37 +0100

    feature: add create command

--- packages/markdown-preview-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/markdown-preview-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,5 +1,7 @@
 import * as Render from '../Render/Render.ts'
+import * as Create2 from '../Create2/Create2.ts'
 
 export const commandMap = {
+  'Webview.create': Create2.create2,
   'MarkdownPreview.render': Render.render,
 }
---
+++ packages/markdown-preview-worker/src/parts/Create2/Create2.ts
@@ -0,0 +1,9 @@
+import * as Render from '../Render/Render.ts'
+import * as Rpc from '../Rpc/Rpc.ts'
+
+export const create2 = async ({ port, savedState, webViewId, uri }) => {
+  const content = await Rpc.invoke('WebView.readFile', uri)
+  const html = await Render.render(content)
+  await port.invoke('initialize', html)
+  return {}
+}
